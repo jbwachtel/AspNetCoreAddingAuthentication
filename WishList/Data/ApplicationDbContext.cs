f486531c1e96e29756f42fa488eea38cdb3e9dde
jbwachtel <89084916+jbwachtel@users.noreply.github.com>
2021-11-16 12:00:14 -0500

    fix error

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using WishList.Models;
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationDbContext>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
